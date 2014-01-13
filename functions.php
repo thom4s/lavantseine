7a7e009b2500a295cbdbc14bee162657461d1334
Thomas <thomasflorentin@yahoo.fr>
2014-01-13 11:18:08 +0100

    js files to one

--- functions.php
+++ functions.php
@@ -119,12 +119,12 @@ add_action( 'widgets_init', 'lavantseine_widgets_init' );
  */
 function lavantseine_scripts() {
 	wp_enqueue_style( 'lavantseine-style', get_template_directory_uri() . '/styles/style.css' );
-	wp_enqueue_style( 'lavantseine-custom', get_template_directory_uri() . '/styles/custom.css' );
+	// wp_enqueue_style( 'lavantseine-custom', get_template_directory_uri() . '/styles/custom.css' );
 
 	wp_enqueue_script( 'lavantseine-scripts', get_template_directory_uri() . '/js/scripts.js', array(), false, true );
-	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.js', array(), false, false );
-	wp_enqueue_script( 'salvatorre', get_template_directory_uri() . '/js/salvatorre.js', array(), false, true );
-	wp_enqueue_script( 'bxslider', get_template_directory_uri() . '/js/bxslider.js', array(), false, true );
+	// wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.js', array(), false, false );
+	// wp_enqueue_script( 'salvatorre', get_template_directory_uri() . '/js/salvatorre.js', array(), false, true );
+	wp_enqueue_script( 'bxslider-modernizr-salvatorre', get_template_directory_uri() . '/js/bxslider.js', array(), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
